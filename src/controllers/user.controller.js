4d52de77ceb97301ff631e7811715f3d8caa1266
Rohit <kumarrohitjamui305@gmail.com>
2026-06-21 17:14:34 +0530

    uploaded images and avatar successfully and bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
-import {User} from "../models/user.routes.js";
+import {User} from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 
@@ -20,7 +20,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
 
     const {fullname, email, username, password} = req.body
-    console.log("email: ", email);
+    // console.log("email: ", email);
 
     if(
         [fullname, email, username, password].some((field) =>
@@ -29,7 +29,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username}, {email}]
     })
 
@@ -38,7 +38,12 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
@@ -74,6 +79,8 @@ const registerUser = asyncHandler(async (req, res) => {
     )
 
 })
+
+
 
 export {registerUser}
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,11 +54,10 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
     this.password = await bcrypt.hash(this.password, 10)
-    next()
+    next
 })
 
-userSchema.methods.isPasswordCorrect = async function 
-(password){
+userSchema.methods.isPasswordCorrect = async function (password){
     return await bcrypt.compare(password, this.password)
 }
 
@@ -68,7 +67,7 @@ userSchema.methods.generateAccessToken = function(){
             _id: this._id,
             email: this.email,
             username: this.username,
-            fullName: this.fullname
+            fullname: this.fullname
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
