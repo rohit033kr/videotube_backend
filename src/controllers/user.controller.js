7d6fb6ac3b35bb3968ec7937955254c8ba04bbb7
Rohit <kumarrohitjamui305@gmail.com>
2026-06-24 01:32:49 +0530

    add changeCurrentPassword, getCurrentUser, and updateAccountDetails functions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -236,6 +236,39 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 })
 
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+    const {oldPassword, newPassword} = req.body
+
+    const user =await User.findById(req.user?.id)
+    const isPasswordCorrect = await user. isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old Password")
+    }
+
+    user.password = newPassword 
+    await user.save({validateBeforeSave: false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password Changed Successfully"))
+
+})
+
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res
+    .status(200)
+    .json(200, req.user, "current user fetched successfully")
+
+})
+
+const updateAccountDetails =  asyncHandler(async(req, res) => {
+    const{fullName, email} = req.body 
+    if(!fullName || !email) {
+        throw new ApiError(400, "All fields are reqired") 
+    }
+})
+
 export {
     registerUser,
     loginUser,
